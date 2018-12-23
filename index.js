91bf03096f618d4c146e0fe40365b3bbfd95bc4b
pbca26 <pbca26@gmail.com>
2018-12-23 13:32:15 +0300

    electron v4.0.0 fix

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('crypto').createHash
+var _createHash = require('crypto').createHash
+var getHashes = require('crypto').getHashes
+var rmd160 = require('ripemd160')
+
+function createHash(algo) {
+  if ((algo === 'rmd160' || algo === 'ripemd160') && getHashes().indexOf('rmd160') === -1) {
+    return new rmd160()
+  } else {
+    return _createHash(algo)
+  }
+}
+
+module.exports = createHash
